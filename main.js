7af0f4bd756044c87bade467661825f73561ec8c
Raghav-Saxena22 <74664397+Raghav-Saxena22@users.noreply.github.com>
2021-08-03 16:39:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav")
+	collect_coin = loadSound("coin.wav")
+	gameover = loadSound("gameover.wav")
+	kick = loadSound("kick.wav")
+	mario_die = loadSound("mariodie.wav")
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent("canvas");
+	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+	video.size(600, 300);
+video.parent("game_console");
+
+poseNet = ml5.poseNet(video, modalLoaded);
+poseNet.on("pose", gotPoses)
+}
+
+function modalLoaded()
+{
+	console.log("Modal Loaded");
+}
+	
+function gotPoses(results)
+{
+if(results.length > 0 )
+{
+	console.log(results)
+	noseX = results[0].pose.nose.x;
+	noseY = results[0].pose.nose.y;
+}
+}
+
+function draw() {
+	game()
+    
+}
+
+
+
+
+
+
